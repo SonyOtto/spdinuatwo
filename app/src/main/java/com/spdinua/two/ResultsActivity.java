b50c469d22b41417d3eb4436afc28704d1d824f7
SonyOtto <o.m.shevelov@gmail.com>
2019-01-09 20:22:39 +0200

    All data from DB now dosplyed correctly, corresponding to entered article number. +1 library in the app

--- app/src/main/java/com/spdinua/two/ResultsActivity.java
+++ app/src/main/java/com/spdinua/two/ResultsActivity.java
@@ -6,9 +6,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 
 public class ResultsActivity extends AppCompatActivity {
 
+    private TextView mTextViewResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,14 +27,45 @@ public class ResultsActivity extends AppCompatActivity {
         String basisDB = "basisDB";
         String termDB = "termDB";
         String noteDB = "noteDB";
+        String message = intent.getStringExtra(SearchActivity.EXTRA_MESSAGE);
 
-        // Displaying NAME parameter from DataBase
-        TextView textView = findViewById(R.id.textView4);
-        String text1 = getString(R.string.NamePart1, nameDB);
-        textView.setText(text1);
+        mTextViewResult = findViewById(R.id.textView4);
+        OkHttpClient client = new OkHttpClient();
+        String DBlink = "http://www.strokypozovnoidavnosti.in.ua/api/product/read_one.php?article=";
+        String url = DBlink + message;
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    final String myResponse = response.body().string();
+
+                    ResultsActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mTextViewResult.setText(myResponse);
+                        }
+                    });
+                }
+            }
+        });
+
+
+        // Displaying NAME, BASIS, TERM AND NOTE parameters from DataBase
+        //TextView textView = findViewById(R.id.textView4);
+        //String text1 = getString(R.string.NamePart1, nameDB);
+        //textView.setText(text1);
         TextView textView2 = findViewById(R.id.textView6);
         String text2 = getString(R.string.BasisTermsNote, basisDB, termDB, noteDB);
         textView2.setText(text2);
-
     }
 }
--- app/src/main/java/com/spdinua/two/SearchActivity.java
+++ app/src/main/java/com/spdinua/two/SearchActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SearchActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.spdinua.two.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +18,9 @@ public class SearchActivity extends AppCompatActivity {
     /** Called when the user taps the next_button on SearchActivily layout */
     public void OpenResultsView(View view) {
         Intent intent = new Intent(this, ResultsActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText3);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 }
