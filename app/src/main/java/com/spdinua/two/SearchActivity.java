8d1e18e5d3617fa6482b13b6cfe2d592bf9be806
SonyOtto <o.m.shevelov@gmail.com>
2019-05-04 22:45:21 +0300

    Check for null value added

--- app/src/main/java/com/spdinua/two/SearchActivity.java
+++ app/src/main/java/com/spdinua/two/SearchActivity.java
@@ -26,9 +26,15 @@ public class SearchActivity extends AppCompatActivity {
         EditText editText = (EditText) findViewById(R.id.editText3);
         String message = editText.getText().toString();
 
-        Integer int2 = Integer.parseInt(editText.getText().toString());
-
-       if (message.matches("[^0-9]+") || message.length() <3 || message.length() >5) {
+        //Check for null string
+        Integer int2 = 1;
+                try {
+                    int2 = Integer.parseInt(editText.getText().toString());
+                } catch (NumberFormatException nfe) {
+                    nfe.printStackTrace();
+                }
+       //Check for numbers only, message length from 4 to 5, input in range from 655 to 1143 (DB limits)
+                if (message.matches("[^0-9]+") || message.length() <3 || message.length() >5) {
             editText.setError("Введіть тільки цифри. Мінімум 3.");
             } else {
            if (int2 < 655 || int2 > 1143) {
